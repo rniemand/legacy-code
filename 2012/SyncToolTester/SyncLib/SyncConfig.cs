9148a404a4caba7d64c48ec2ad45d5ea74ce7a81
Richard Niemand <niemand.richard@gmail.com>
2022-06-07 23:26:35 -0600

    Added SyncToolTester

---
+++ 2012/SyncToolTester/SyncLib/SyncConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Rn.Core.Helpers;
+using Rn.Core.Logging;
+
+namespace SyncLib
+{
+    public static class SyncConfig
+    {
+        private static readonly string ConfigFilePath;
+        private static XmlDocument _configXml;
+
+        static SyncConfig()
+        {
+            // Check for the config file
+            ConfigFilePath = String.Format("{0}SyncConfig.xml", IOHelper.BaseDir);
+            
+            if (File.Exists(ConfigFilePath))
+            {
+                Locale.LogEvent("sync_lib", "0001", ConfigFilePath);
+                Environment.Exit(-1);
+            }
+
+            LoadConfigFile();
+        } 
+
+
+        private static void LoadConfigFile()
+        {
+            try
+            {
+                _configXml = new XmlDocument();
+                _configXml.Load(ConfigFilePath);
+            }
+            catch (Exception ex)
+            {
+                Locale.LogEvent("rn.core.common", "0002", ex.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        public static void GetActiveSyncs()
+        {
+            
+        }
+
+
+    }
+
+}
---
+++ 2012/SyncToolTester/Testet/SyncToolTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SyncLib;
+
+namespace SyncToolTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SyncConfig.GetActiveSyncs();
+
+
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+    }
+}
